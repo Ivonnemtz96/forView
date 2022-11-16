ac89357e56afa89f982e9070f4e8d67a9c55c01b
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-16 16:58:07 -0700

    Se agrega info en "empresa"

--- modules/empresa.php
+++ modules/empresa.php
@@ -15,12 +15,16 @@
     <div class="container">
         <div class="row mb-5">
             <div class="col-md-4 animate-box" data-animatitulote-effect="fadeInUp">
-                <div class="sub-title border-bot-light">Titulo 1</div>
+                <div class="sub-title border-bot-light">ForView</div>
             </div>
             <div class="col-md-8 animate-box" data-animate-effect="fadeInUp">
-                <div class="section-title"><span>Titulo 2</span></div>
+                <div class="section-title"><span>ForView</span></div>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum velit nobis laboriosam distinctio quo saepe qui voluptatibus numquam quibusdam vel magni optio, dolores labore recusandae laborum accusantium veritatis rem officia.
+                    La experiencia madurada en la empresa de familia de instalación de puertas y ventanas a lado de un
+                    padre ingeniero civil reconocido y estimado en el sector, la pasión por la mecánica de los autos ATR
+                    de carrera, fundamentada en la tecnología y la automatización de procesos para alcanzar la calidad y
+                    precisión deseada, con su obsesión por mejorar constantemente como profesional, ser humano y
+                    empresario han llevado a Jose Humberto Armenta a fundar en 2022 For View Doors & Windows.
                 </p>
             </div>
         </div>
@@ -36,7 +40,10 @@
                         <!-- <h6>Project P.01</h6> -->
                         <h3>Titulo 3</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero dolore labore, maxime cum facilis omnis tenetur nihil temporibus ad at, quisquam itaque illo quis modi beatae repudiandae incidunt, hic atque. Commodi, a tempore fugiat id culpa veniam maxime quia.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus libero dolore labore,
+                            maxime cum facilis omnis tenetur nihil temporibus ad at, quisquam itaque illo quis modi
+                            beatae repudiandae incidunt, hic atque. Commodi, a tempore fugiat id culpa veniam maxime
+                            quia.
                         </p>
                         <!-- <div class="more"><a href="project-page.html" class="link-btn" tabindex="0">View Project</a>
                         </div> -->
@@ -51,7 +58,10 @@
                         <!-- <h6>Project P.02</h6> -->
                         <h3>Titulo 4</h3>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptates aspernatur similique iusto animi autem dolor eos maiores hic consectetur magni ipsum sapiente veniam mollitia ducimus at soluta officiis temporibus, culpa rerum, est itaque voluptatibus asperiores! Distinctio obcaecati iste officiis.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptates aspernatur
+                            similique iusto animi autem dolor eos maiores hic consectetur magni ipsum sapiente veniam
+                            mollitia ducimus at soluta officiis temporibus, culpa rerum, est itaque voluptatibus
+                            asperiores! Distinctio obcaecati iste officiis.
                         </p>
                         <!-- <div class="more"><a href="project-page.html" class="link-btn" tabindex="0">View Project</a>
                         </div> -->
@@ -76,7 +86,10 @@
                         <!-- <h6>Project P.03</h6> -->
                         <h3>Titulo 5</h3>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolores? Iure nobis est obcaecati hic suscipit, porro velit animi fugit, iusto eligendi perferendis a quibusdam quam aut esse. Quae aliquid doloremque optio? Molestias laboriosam ea facilis odit cumque quos quo!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolores? Iure nobis est
+                            obcaecati hic suscipit, porro velit animi fugit, iusto eligendi perferendis a quibusdam quam
+                            aut esse. Quae aliquid doloremque optio? Molestias laboriosam ea facilis odit cumque quos
+                            quo!
                         </p>
                         <!-- <div class="more"><a href="project-page.html" class="link-btn" tabindex="0">View Project</a>
                         </div> -->
@@ -96,11 +109,22 @@
                 <div class="sub-title border-bot-light">Filosofía</div>
             </div>
             <div class="col-md-8 animate-box" data-animate-effect="fadeInUp">
-                <div class="section-title"><span>Titulo</span> 6</div>
+                <h2 class="section-title"><span>ForView</span></h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quisquam odit sit laudantium provident neque minus fugiat possimus eum! Sint, blanditiis! Maiores, assumenda dicta. Tenetur ut placeat pariatur inventore. Quaerat.
+                    La realización de las puertas y ventanas conllevan acciones antiguas del Hombre:
+                    Escuchar, Imaginar, Solucionar problemas, Conceptualizar, Medir, Cargar, Instalar
                     <br><br>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci sequi exercitationem facilis cum quia aperiam aspernatur voluptatum eius ratione ipsa! In autem vel recusandae ut laudantium aperiam, error est nisi ad facere illum libero numquam porro minus, modi officia praesentium!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci sequi exercitationem facilis cum
+                    quia aperiam aspernatur voluptatum eius ratione ipsa! In autem vel recusandae ut laudantium aperiam,
+                    error est nisi ad facere illum libero numquam porro minus, modi officia praesentium!
+                </p>
+                <h4><span>Y acciones que ejecuta la tecnología:</span></h4>
+                <p>
+                    Calcular, Dibujar, transportar, cortar, atornillar, barrenar, pintar, sellar, vidriar. <br><br>
+                    Cualquier acción implica esfuerzo físico y emocional en menor o mayor medida. Involucrarse no solo
+                    físicamente si no emotivamente en lo que se hace y hacer que el resultado deseado sea por si mismo
+                    una recompensa del esfuerzo hecho, por todo el equipo de ForView.
+
                 </p>
             </div>
         </div>
