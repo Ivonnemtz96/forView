1cfc8d09442875e1ee110fe4774866210e9e564c
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-17 13:14:48 -0700

    Se edita footer, se quita distribuidor, se agrega link al correo

--- includes/footer.php
+++ includes/footer.php
@@ -9,9 +9,12 @@
                 <div class="col-md-4">
                     <div class="item">
                         <h3 style="color: white;">San José</h3>
-                        <p style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptate.</p>
+                        <p style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+                            voluptate.</p>
                         <p style="color: white;" class="phone">+52 123 456 7890</p>
-                        <p style="color: white;" class="mail">info@forview.com</p>
+                        <p style="color: white;" class="mail">
+                            <a style="color: #fff;" target="_blank" href="mailto:info@forviewdoors.com">info@forviewdoors.com</a>
+                        </p>
                         <div class="social mt-2">
                             <a style="color: white;" href="#"><i class="ti-twitter"></i></a>
                             <a style="color: white;" href="#"><i class="ti-instagram"></i></a>
@@ -26,7 +29,7 @@
                             <li><a style="color: white;" href="productos.php">Productos</a></li>
                             <li><a style="color: white;" href="galeria.php">Galería</a></li>
                             <li><a style="color: white;" href="empresa.php">Empresa</a></li>
-                            <li><a style="color: white;" href="distribuidor.php">Distribuidor</a></li>
+                            <!-- <li><a style="color: white;" href="distribuidor.php">Distribuidor</a></li> -->
                             <li><a style="color: white;" href="descarga.php">Descarga</a></li>
                         </ul>
                     </div>
@@ -38,7 +41,8 @@
         <div class="container">
             <div class="row text-center">
                 <div class="col-md-12">
-                    <p>© 2022 ForView. Diseñado por <a target="_blank" href="https://bananagroup.mx"><b>Banana Group Marketing</b></a>.</p>
+                    <p>© 2022 ForView. Diseñado por <a target="_blank" href="https://bananagroup.mx"><b>Banana Group
+                                Marketing</b></a>.</p>
                 </div>
             </div>
         </div>
