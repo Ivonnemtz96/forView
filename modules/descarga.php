f31414996088b3373c4b37208e02a5b55786ca52
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-12 15:23:20 -0700

    Descarga Ok

--- modules/descarga.php
+++ modules/descarga.php
@@ -1,34 +1,39 @@
 <!-- Header Banner -->
-<div class="banner-header valign bg-img bg-fixed" data-background="images/slider/2.jpg">
+<div class="banner-header valign bg-img bg-fixed" data-background="images/slider/2.jpg" style="background-position: center center;">
     <div class="container">
         <div class="row">
             <div class="col-md-12 caption mt-60">
-                <div class="subtitle">Solicitud de Docucumentación</div>
-                <div class="title">Escríbenos</div>
+                <div style="background-color: #fff; width: fit-content; padding: 7px; margin-left: 0;" class="subtitle">
+                    Solicitud de documentación
+                </div>
+                <br>
+                <div style="background-color: #fff;color:#000; width: fit-content; padding: 7px;" class="title">
+                    <strong>Escríbenos</strong>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
 <!-- Contact -->
-<div class="contact section-padding"  style="padding-bottom: 0;">
+<div class="contact section-padding" style="padding-bottom: 0;">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
                 <!-- Contact Info -->
                 <h4 class="mb-4">San José </h4>
-                <p>Lorem ipsum dolor sit amet <br> consectetur adipisicing elit. Adipisci, quod!</p>
-                <div class="phone">+52 123 456 7890</div>
-                <div class="mail mb-3">info@forview.com</div>
-                <div class="social mt-2">
+                <p>Guadalupe Victoria 3525, Sta Rosa, 23428 San José del Cabo, B.C.S.</p>
+                <div class="phone"><a class="phone1" href="tel:+526241425552">+52 624 142 5552</a></div>
+                <div class="mail mb-3"><a class="phone1" href="mailto:info@forviewdoors.com">info@forviewdoors.com</a></div>
+                <div hidden class="social mt-2">
                     <a href="#"><i class="ti-twitter"></i></a>
                     <a href="#"><i class="ti-instagram"></i></a>
                     <a href="#"><i class="ti-linkedin"></i></a>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-2">
                 <!-- Contact Info -->
-                <h4 class="mb-4">San José </h4>
+                <!-- <h4 class="mb-4">San José </h4>
                 <p>Lorem ipsum dolor sit amet <br> consectetur adipisicing elit. Adipisci, quod!</p>
                 <div class="phone">+52 123 456 7890</div>
                 <div class="mail mb-3">info@forview.com</div>
@@ -36,20 +41,22 @@
                     <a href="#"><i class="ti-twitter"></i></a>
                     <a href="#"><i class="ti-instagram"></i></a>
                     <a href="#"><i class="ti-linkedin"></i></a>
-                </div>
+                </div> -->
             </div>
             <!-- form -->
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <h4 class="mb-4">Solicitud de documentación</h4>
                 <p>
                     Si desea recibir los archivos DWG / DXF / BIM o Revit, rellene este formulario. <br>
-                    No dude en trasladarnos su proyecto o enviarnos su solicitud de la forma más detallada posible; esto nos ayudará a satisfacer sus necesidades de la mejor forma posible.
+                    No dude en trasladarnos su proyecto o enviarnos su solicitud de la forma más detallada posible; esto
+                    nos ayudará a satisfacer sus necesidades de la mejor forma posible.
                 </p>
                 <form method="post" class="contact__form" action="mail.php">
                     <!-- Form message -->
                     <div class="row">
                         <div class="col-12">
-                            <div class="alert alert-success contact__msg" style="display: none" role="alert"> Mensaje enviado</div>
+                            <div class="alert alert-success contact__msg" style="display: none" role="alert"> Mensaje
+                                enviado</div>
                         </div>
                     </div>
                     <!-- Form elements -->
@@ -67,7 +74,8 @@
                             <input name="subject" type="text" placeholder="Asunto *" required="">
                         </div>
                         <div class="col-md-12 form-group">
-                            <textarea name="message" id="message" cols="30" rows="4" placeholder="Mensaje *" required=""></textarea>
+                            <textarea name="message" id="message" cols="30" rows="4" placeholder="Mensaje *"
+                                required=""></textarea>
                         </div>
                         <div class="col-md-12 mt-2">
                             <input name="submit" type="submit" value="Enviar">
@@ -88,7 +96,7 @@
             </div>
             <div class="col-md-8 animate-box" data-animate-effect="fadeInUp">
                 <!-- <div class="section-title"><span>Titulo</span></div> -->
-                <h4>Manuales</h4>
+                <h4>Manuales PDF</h4>
                 <hr>
                 <button class="btn-descarga"><i class="fa fa-download"></i> IT</button>
                 <button class="btn-descarga"><i class="fa fa-download"></i> EN </button>
@@ -97,7 +105,7 @@
             <div class="col-md-4"></div>
             <div class="col-md-8 animate-box mt-5" data-animate-effect="fadeInUp">
                 <!-- <div class="section-title"><span>Titulo</span></div> -->
-                <h4>Titulo 4</h4>
+                <h4>Brochure PDF</h4>
                 <hr>
                 <button class="btn-descarga"><i class="fa fa-download"></i> IT</button>
                 <button class="btn-descarga"><i class="fa fa-download"></i> EN </button>
@@ -108,5 +116,5 @@
 </section>
 <!-- Maps -->
 <div class="google-maps">
-    <iframe id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5265.917123113766!2d-109.71107597108902!3d23.090442179616932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af51532c7132c7%3A0xf09c9724da511bbd!2sBanana%20Marketing%20Group!5e0!3m2!1sen!2smx!4v1668181287553!5m2!1sen!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d952.1620757883456!2d-109.707524396149!3d23.086605764256475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af5a58883c5d77%3A0xec9ae02d75b666e4!2sCALVALUM!5e0!3m2!1ses-419!2smx!4v1670883688750!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
