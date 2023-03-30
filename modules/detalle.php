fc93ac92a07470397ddf61f55503c7b86c41ab80
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-30 12:29:23 -0700

    proyecto individual con cambios

--- modules/detalle.php
+++ modules/detalle.php
@@ -4,24 +4,36 @@
         <div class="row">
             <div class="col-md-12 caption mt-60">
                 <div class="subtitle"><a href="productos.php">Detalles</a></div>
-                <div class="title">Guillotina</div>
+                <div class="title">Correderas</div>
             </div>
         </div>
     </div>
 </div>
 <!-- Project Page  -->
 <section class="project-page section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
+    <div class="container-fluid p-5">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <span>Producto</span>
+                <h2>Corredera</h2>
+                <h2></h2>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit blanditiis optio facilis itaque quaerat commodi nobis iste exercitationem quia deleniti, a nam tempore aliquam quae, illum ex, expedita culpa harum! Facilis iste, nulla eligendi commodi ipsam blanditiis? Nobis praesentium beatae nihil unde perferendis aliquam delectus, reprehenderit, dolor odio maxime amet eos ipsa veritatis asperiores autem expedita nulla aspernatur! Consectetur, libero.
+                    El sistema corredizo permite acceder a una amplia vista panorámica, con distintas configuraciones de
+                    hojas y aperturas. Su diseño minimalista permite embutir el marco en el piso sin generar un cambio
+                    de nivel, el cual al realizar una transición entre dos espacios pasa desapercibido al tacto, y a su
+                    vez, se puede guardar las hojas en un cajón de muro, permitiendo despejar completamente el espacio.
                 </p><br>
+                <p>
+                    De igual forma cuenta con soluciones anticiclónicas, contemplando la configuración de los refuerzos
+                    y dimensiones del elemento, su facilidad de movimiento es posible debido a que los rodamientos son
+                    de acero inoxidable fabricados en ForView. <br> <br>
+                    El diseño arquitectónico minimalista de los canceles permiten ocultar de la vista la tornillería, al
+                    igual que sellar completamente los cristales, con espesores anodizados para los perfiles desde 20
+                    micras. Todas estas características aportan elegancia, robustez y practicidad a estas soluciones de
+                    puertas corredizas.
+                </p>
             </div>
-        </div>
-        <!-- project slider -->
-        <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-md-6">
                 <div class="owl-carousel owl-theme">
                     <div class="portfolio-item"> <img class="img-fluid" src="images/slider/4.jpg" alt=""> </div>
                     <div class="portfolio-item"> <img class="img-fluid" src="images/slider/5.jpg" alt=""> </div>
@@ -29,55 +41,153 @@
                     <div class="portfolio-item"> <img class="img-fluid" src="images/slider/7.jpg" alt=""> </div>
                     <div class="portfolio-item"> <img class="img-fluid" src="images/slider/8.jpg" alt=""> </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="project-bar">
-                            <div class="row justify-content-between align-items-center text-left text-lg-start">
-                                <div class="col-md-3 mb-15">
-                                    <h5>Año</h5>
-                                    <h6>2020 - 2022</h6>
-                                </div>
-                                <div class="col-md-3 mb-15">
-                                    <h5>Compañía</h5>
-                                    <h6>ForView</h6>
-                                </div>
-                                <div class="col-md-3 mb-15">
-                                    <h5>Nombre</h5>
-                                    <h6>Nombre de producto</h6>
-                                </div>
-                                <div class="col-md-3 mb-15">
-                                    <h5>Dirección</h5>
-                                    <h6>San José del Cabo</h6>
-                                </div>
-                            </div>
-                        </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container p-5">
+        <!-- project slider -->
+        <div class="row mt-5">
+            <h2><b>Proyectos</b></h2>
+            <div class="col-md-12">
+                <p>
+                    Te mostramos los proyectos en los que hemos sido parte de cada detalle y profesionalismo.
+                </p>
+                <br>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay / Villa 221. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/chilenos.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/chileno2.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/chileno3.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/chileno4.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Palmilla Villa 381 / Villa 221. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/palmilla1.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/palmilla2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay Re 28 / Villa 221. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/chilenoRe2.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/ChilenoRe1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Waldorf Astoria Hotel. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/waldorf1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Querencia / Casa Legado. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/querencia1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay/ Villa HV 305. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/chilenohv1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Diamante / Shangri LA 319. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/diamante.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="images/projects/diamante2.jpg" alt="">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
+    </div>
+</section>
+
+<section class="section-padding">
+    <div class="container">
+        <div class="row mb-5 animate-box" data-animate-effect="fadeInUp">
             <div class="col-md-12">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius obcaecati officia placeat praesentium facere accusamus voluptatibus, excepturi magnam a itaque vitae rem maxime, modi accusantium, quam inventore asperiores. Distinctio, reprehenderit in! Reiciendis quo ad adipisci exercitationem natus provident corrupti nostrum.
-                </p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus illo in iure reiciendis quam, facilis esse, incidunt ea quasi architecto odit. Ad dolorum illum amet asperiores ea ut expedita, debitis facere animi atque possimus. Aliquam impedit quaerat earum quis blanditiis distinctio maiores sapiente, mollitia, ut necessitatibus doloribus eius eligendi modi voluptas sint est autem consequuntur? Rem, incidunt! Quo iure blanditiis quas enim fuga in modi, velit illum? Deserunt quaerat animi non recusandae, accusantium veritatis vel unde tempora nihil voluptatem autem eius voluptatum velit dicta quis maiores explicabo porro. Labore sequi reprehenderit libero ea numquam harum pariatur officiis nisi aliquam officia!</p><br>
-                <ul class="list-unstyled page-list mb-30">
-                    <li>
-                        <div class="page-list-icon"> <span class="ti-check"></span> </div>
-                        <div class="page-list-text">
-                            <p>Titulo</p>
+                <ul class="accordion-box clearfix">
+                    <li class="accordion block">
+                        <div class="acc-btn">Modern Architectural Structures</div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">Architecture viverra tristique justo duis vitae diam neque nivamus
+                                    aestan ateuene artines aringianu atelit finibus viverra nec lacus. Nedana theme
+                                    erodino setlie suscipe no tristique.</div>
+                            </div>
                         </div>
                     </li>
-                    <li>
-                        <div class="page-list-icon"> <span class="ti-check"></span> </div>
-                        <div class="page-list-text">
-                            <p>Titulo</p>
+                    <li class="accordion block">
+                        <div class="acc-btn">Modern Building Structures</div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">Architecture viverra tristique justo duis vitae diam neque nivamus
+                                    aestan ateuene artines aringianu atelit finibus viverra nec lacus. Nedana theme
+                                    erodino setlie suscipe no tristique.</div>
+                            </div>
                         </div>
                     </li>
-                    <li>
-                        <div class="page-list-icon"> <span class="ti-check"></span> </div>
-                        <div class="page-list-text">
-                            <p>Titulo</p>
+                    <li class="accordion block">
+                        <div class="acc-btn">Modern Design Structures</div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">Architecture viverra tristique justo duis vitae diam neque nivamus
+                                    aestan ateuene artines aringianu atelit finibus viverra nec lacus. Nedana theme
+                                    erodino setlie suscipe no tristique.</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="accordion block">
+                        <div class="acc-btn">Modern Urban Structures</div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">Architecture viverra tristique justo duis vitae diam neque nivamus
+                                    aestan ateuene artines aringianu atelit finibus viverra nec lacus. Nedana theme
+                                    erodino setlie suscipe no tristique.</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="accordion block">
+                        <div class="acc-btn">Modern Interior Structures</div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">Architecture viverra tristique justo duis vitae diam neque nivamus
+                                    aestan ateuene artines aringianu atelit finibus viverra nec lacus. Nedana theme
+                                    erodino setlie suscipe no tristique.</div>
+                            </div>
                         </div>
                     </li>
                 </ul>
