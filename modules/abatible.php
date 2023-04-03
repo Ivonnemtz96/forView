5c2d090dfb8e48c2daf89f4c73e8e483624dd497
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-03 11:06:43 -0700

    abatible

---
+++ modules/abatible.php
@@ -0,0 +1,244 @@
+<!-- Header Banner -->
+<div class="banner-header valign bg-img bg-fixed" data-background="/images/slider/2.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 caption mt-60">
+                <div class="subtitle"><a href="/productos">Detalles</a></div>
+                <div class="title">Abatible</div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Project Page  -->
+<section class="project-page section-padding">
+    <div class="container-fluid p-5">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <span>Producto</span>
+                <h2>Abatible</h2>
+                <h2></h2>
+                <p>
+                <ul>
+                    <li>Proyectante, vasista, oscilobatiente y batiente.</li>
+                    <li>Los sistemas pueden abrir en los dos sentidos.</li>
+                    <li>Las aleaciones y la robustez de nuestros perfiles garantizan una hermeticidad total.</li>
+                    <li>Las distintas configuraciones de cristal permitidas en nuestros sistemas ofrecen soluciones en
+                        condiciones climáticas extremas.</li>
+                    <li>Los mecanismos de cierre y apertura son ajustados acorde a los parámetros requeridos.</li>
+
+                </ul>
+                </p><br>
+            </div>
+            <div class="col-md-6">
+                <div class="fotorama" data-width="100%" data-ratio="800/600" data-minwidth="400" data-maxwidth="1000"
+                    data-minheight="300" data-maxheight="100%">
+                    <img class="img-fluid" src="/images/slider/batiente.jpg" alt=""> 
+                    <img class="img-fluid" src="/images/slider/batienteV.jpg" alt=""> 
+                    <img class="img-fluid" src="/images/slider/batiente3.jpg" alt=""> 
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div hidden class="container p-5">
+        <!-- project slider -->
+        <div class="row mt-5">
+            <h2><b>Proyectos</b></h2>
+            <div class="col-md-12">
+                <p>
+                    Te mostramos los proyectos en los que hemos sido parte de cada detalle y profesionalismo.
+                </p>
+                <br>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay / Villa 221. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenos.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno2.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno3.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno4.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Palmilla Villa 381 / Villa 221. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/palmilla1.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/palmilla2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay Re 28 / Villa 221. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenoRe2.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/ChilenoRe1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Waldorf Astoria Hotel. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/waldorf1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Querencia / Casa Legado. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/querencia1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay/ Villa HV 305. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenohv1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Diamante / Shangri LA 319. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/diamante.jpg" alt="">
+                    </div>
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/diamante2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section-padding">
+    <div class="container">
+        <div class="row mb-5 animate-box" data-animate-effect="fadeInUp">
+            <div class="col-md-12">
+                <ul class="accordion-box clearfix">
+                    <li class="accordion block">
+                        <div class="acc-btn">
+                            <h2>ForView 28 / Puerta batiente</h2>
+                        </div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">
+                                    <p style="color: #000;">
+                                    <ul>
+                                        <li>Proyectante, vasista, oscilobatiente y batiente.</li>
+                                        <li>Los sistemas pueden abrir en los dos sentidos.</li>
+                                        <li>Las aleaciones y la robustez de nuestros perfiles garantizan una
+                                            hermeticidad total.</li>
+                                        <li>Las distintas configuraciones de cristal permitidas en nuestros sistemas
+                                            ofrecen soluciones en
+                                            condiciones climáticas extremas.</li>
+                                        <li>Los mecanismos de cierre y apertura son ajustados acorde a los parámetros
+                                            requeridos.</li>
+
+                                    </ul>
+                                    </p>
+                                    <p>
+                                    <h4>Configuración</h4>
+                                    <ol style="color: #000;">
+                                        <li>Marco principal</li>
+                                        <li>Zoclo</li>
+                                        <li>Maneta</li>
+                                        <li>Sistema cerradura</li>
+                                        <li>Cubre pijas</li>
+                                        <li>Sello</li>
+                                        <li>Vinil guardapolvo</li>
+                                        <li>Reductor</li>
+                                        <li>Cierra puertas oculto</li>
+                                        <li>Bisagra</li>
+                                    </ol>
+                                    </p>
+                                    <div class="container mb-5">
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <span>ForView 28 / Puerta batiente</span> <br>
+                                                <img style="width: 20rem;" src="/images/abatible28-d.png" alt="">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-dark mt-3">
+                                    Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="accordion block">
+                        <div class="acc-btn">
+                            <h2>ForView 28 / Ventana batiente</h2>
+                        </div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">
+                                    <p style="color: #000;">
+                                    <ul>
+                                        <li>Proyectante, vasista, oscilobatiente y batiente.</li>
+                                        <li>Los sistemas pueden abrir en los dos sentidos.</li>
+                                        <li>Las aleaciones y la robustez de nuestros perfiles garantizan una
+                                            hermeticidad total.</li>
+                                        <li>Las distintas configuraciones de cristal permitidas en nuestros sistemas
+                                            ofrecen soluciones en
+                                            condiciones climáticas extremas.</li>
+                                        <li>Los mecanismos de cierre y apertura son ajustados acorde a los parámetros
+                                            requeridos.</li>
+
+                                    </ul>
+                                    </p>
+                                    <p>
+                                    <h4>Configuración</h4>
+                                    <ol style="color: #000;">
+                                        <li>Marco principal</li>
+                                        <li>Marco ventana</li>
+                                        <li>Maneta</li>
+                                        <li>Sistema cerradura</li>
+                                        <li>Cubre pijas</li>
+                                        <li>Sello</li>
+                                        <li>Reductor</li>
+                                        <li>Bisagra</li>
+                                    </ol>
+                                    </p>
+                                    <div class="container mb-5">
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <span>ForView 28 / Ventana batiente</span> <br>
+                                                <img style="width: 20rem;" src="/images/abatible28-v.png" alt="">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn btn-dark mt-3">
+                                    Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
