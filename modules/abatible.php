a536eab4045c8c7b8e80bb4ac6c098619c83f8ba
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-05 12:17:01 -0700

    abatible ok

--- modules/abatible.php
+++ modules/abatible.php
@@ -4,13 +4,13 @@
         <div class="row">
             <div class="col-md-12 caption mt-60">
                 <div class="subtitle"><a href="/productos">Detalles</a></div>
-                <div  style="background-color: #000; padding: 10px;" class="title">Abatible</div>
+                <div style="background-color: #000; padding: 10px;" class="title">Abatible</div>
             </div>
         </div>
     </div>
 </div>
 <!-- Project Page  -->
-<section class="project-page section-padding">
+<section class="project-page section-padding" style="margin-bottom: 0px;">
     <div class="container-fluid p-5">
         <div class="row align-items-center">
             <div class="col-md-6">
@@ -32,15 +32,15 @@
             <div class="col-md-6">
                 <div class="fotorama" data-width="100%" data-ratio="800/600" data-minwidth="400" data-maxwidth="1000"
                     data-minheight="300" data-maxheight="100%">
-                    <img class="img-fluid" src="/images/slider/batiente.jpg" alt=""> 
-                    <img class="img-fluid" src="/images/slider/batienteV.jpg" alt=""> 
-                    <img class="img-fluid" src="/images/slider/batiente3.jpg" alt=""> 
+                    <img class="img-fluid" src="/images/slider/batiente.jpg" alt="">
+                    <img class="img-fluid" src="/images/slider/batienteV.jpg" alt="">
+                    <img class="img-fluid" src="/images/slider/batiente3.jpg" alt="">
                 </div>
 
             </div>
         </div>
     </div>
-    <div hidden class="container p-5">
+    <div class="container p-5">
         <!-- project slider -->
         <div class="row mt-5">
             <h2><b>Proyectos</b></h2>
@@ -52,77 +52,10 @@
             </div>
             <div class="col-6 col-md-6 mt-4 mb-5">
                 <h3>
-                    <strong>Chileno Bay / Villa 221. Los Cabos, México.</strong>
-                </h3>
-                <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenos.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno2.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno3.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno4.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-md-6 mt-4 mb-5">
-                <h3>
-                    <strong>Palmilla Villa 381 / Villa 221. Los Cabos, México.</strong>
-                </h3>
-                <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/palmilla1.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/palmilla2.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-md-6 mt-4 mb-5">
-                <h3>
-                    <strong>Chileno Bay Re 28 / Villa 221. Los Cabos, México.</strong>
-                </h3>
-                <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenoRe2.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/ChilenoRe1.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-6 col-md-6 mt-4 mb-5">
-                <h3>
-                    <strong>Waldorf Astoria Hotel. Los Cabos, México.</strong>
-                </h3>
-                <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/waldorf1.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-md-6 mt-4 mb-5">
-                <h3>
-                    <strong>Querencia / Casa Legado. Los Cabos, México.</strong>
-                </h3>
-                <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/querencia1.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-md-6 mt-4 mb-5">
-                <h3>
                     <strong>Chileno Bay/ Villa HV 305. Los Cabos, México.</strong>
                 </h3>
                 <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenohv1.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 col-md-6 mt-4 mb-5">
-                <h3>
-                    <strong>Diamante / Shangri LA 319. Los Cabos, México.</strong>
-                </h3>
-                <div class="owl-carousel owl-theme">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/diamante.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/diamante2.jpg" alt="">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/abatible.jpg" alt="">
                     </div>
                 </div>
             </div>
@@ -130,7 +63,7 @@
     </div>
 </section>
 
-<section class="section-padding">
+<section class="section-padding" style="margin-top: 0px;">
     <div class="container">
         <div class="row mb-5 animate-box" data-animate-effect="fadeInUp">
             <div class="col-md-12">
@@ -181,7 +114,8 @@
                                         </div>
                                     </div>
                                 </div>
-                                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf" download="Hoja de datos Abatible">
+                                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf"
+                                    download="Hoja de datos Abatible">
                                     Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
                                 </a>
                             </div>
@@ -231,13 +165,33 @@
                                         </div>
                                     </div>
                                 </div>
-                                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf" download="Hoja de datos Abatible">
+                                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf"
+                                    download="03-Batiente.pdf">
                                     Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
                                 </a>
                             </div>
                         </div>
                     </li>
                 </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="folleto">
+    <div class="container">
+        <div class="row mb-5 animate-box" data-animate-effect="fadeInUp">
+            <div class="col-md-6">
+                <img src="/images/folleto.png" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2>FOLLETO TÉCNICO</h2>
+                <span>Puede conocer sus características técnicas descargando el folleto técnico de ForView</span>
+                <br><br>
+
+                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf" download="03-Batiente.pdf">
+                    Descargar
+                </a>
             </div>
         </div>
     </div>
