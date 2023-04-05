df66dc0305eb95a03c18a5e9432f528d59168f64
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-04-05 13:25:14 -0700

    antihuracán ok

---
+++ modules/antihuracan.php
@@ -0,0 +1,210 @@
+<!-- Header Banner -->
+<div class="banner-header valign bg-img bg-fixed" data-background="/images/slider/2.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 caption mt-60">
+                <div class="subtitle"><a href="/productos">Detalles</a></div>
+                <div style="background-color: #000; padding: 10px;" class="title">Protección anti huracán</div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Project Page  -->
+<section class="project-page section-padding" style="margin-bottom: 0px;">
+    <div class="container-fluid p-5">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <span>Producto</span>
+                <h2>Protección anti huracán</h2>
+                <h2></h2>
+                <p>
+                    El sistema de cortina anti huracán brinda una mayor protección a los canceles expuestos que
+                    conforman la edificación, manteniéndose estable a la resistencia del viento con velocidades mayores
+                    de 250 km/h . <br><br>
+                    Con el sistema de cierre al centro innovador, y sencillo de maniobrar te permite desplegarlo y
+                    fijarlo con una gran facilidad sin requerir de un gran esfuerzo , solo con ajustar los pasadores
+                    sobre el eje superior e inferior la función de fijación queda resuelta.
+                </p><br>
+            </div>
+            <div class="col-md-6">
+                <div class="fotorama" data-width="100%" data-ratio="800/600" data-minwidth="400" data-maxwidth="1000"
+                    data-minheight="300" data-maxheight="100%">
+                    <img class="img-fluid" src="/images/projects/proteccion-1.jpg" alt="">
+                    <img class="img-fluid" src="/images/projects/proteccion2.jpg" alt="">
+                    <img class="img-fluid" src="/images/projects/proteccion3.jpg" alt="">
+                    <img class="img-fluid" src="/images/projects/proteccion4.jpg" alt="">
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div hidden class="container p-5">
+        <!-- project slider -->
+        <div class="row mt-5">
+            <h2><b>Proyectos</b></h2>
+            <div class="col-md-12">
+                <p>
+                    Te mostramos los proyectos en los que hemos sido parte de cada detalle y profesionalismo.
+                </p>
+                <br>
+            </div>
+            <div class="col-6 col-md-6 mt-4 mb-5">
+                <h3>
+                    <strong>Chileno Bay/ Villa HV 305. Los Cabos, México.</strong>
+                </h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/abatible.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="callToAction section-padding">
+    <div class="container animate-box" style="background-color: #000;  padding: 20px;" data-animate-effect="fadeInUp">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-9">
+                <h2 style="color: #fff;">Contáctenos ahora mismo para obtener un diseño personalizado y de alta calidad.</h2>
+            </div>
+            <div class="col-md-3 ">
+                <a type="button" href="/descarga" class="btn btn-light">Descarga</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section hidden class="section-padding" style="margin-top: 0px;">
+    <div class="container">
+        <div class="row mb-5 animate-box" data-animate-effect="fadeInUp">
+            <div class="col-md-12">
+                <ul class="accordion-box clearfix">
+                    <li class="accordion block" id="puerta">
+                        <div class="acc-btn">
+                            <h2>ForView 28 / Puerta batiente</h2>
+                        </div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">
+                                    <p style="color: #000;">
+                                    <ul>
+                                        <li>Proyectante, vasista, oscilobatiente y batiente.</li>
+                                        <li>Los sistemas pueden abrir en los dos sentidos.</li>
+                                        <li>Las aleaciones y la robustez de nuestros perfiles garantizan una
+                                            hermeticidad total.</li>
+                                        <li>Las distintas configuraciones de cristal permitidas en nuestros sistemas
+                                            ofrecen soluciones en
+                                            condiciones climáticas extremas.</li>
+                                        <li>Los mecanismos de cierre y apertura son ajustados acorde a los parámetros
+                                            requeridos.</li>
+
+                                    </ul>
+                                    </p>
+                                    <p>
+                                    <h4>Configuración</h4>
+                                    <ol style="color: #000;">
+                                        <li>Marco principal</li>
+                                        <li>Zoclo</li>
+                                        <li>Maneta</li>
+                                        <li>Sistema cerradura</li>
+                                        <li>Cubre pijas</li>
+                                        <li>Sello</li>
+                                        <li>Vinil guardapolvo</li>
+                                        <li>Reductor</li>
+                                        <li>Cierra puertas oculto</li>
+                                        <li>Bisagra</li>
+                                    </ol>
+                                    </p>
+                                    <div class="container mb-5">
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <span>ForView 28 / Puerta batiente</span> <br>
+                                                <img style="width: 20rem;" src="/images/abatible28-d.png" alt="">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf"
+                                    download="Hoja de datos Abatible">
+                                    Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="accordion block" id="ventana">
+                        <div class="acc-btn">
+                            <h2>ForView 28 / Ventana batiente</h2>
+                        </div>
+                        <div class="acc-content">
+                            <div class="content">
+                                <div class="text">
+                                    <p style="color: #000;">
+                                    <ul>
+                                        <li>Proyectante, vasista, oscilobatiente y batiente.</li>
+                                        <li>Los sistemas pueden abrir en los dos sentidos.</li>
+                                        <li>Las aleaciones y la robustez de nuestros perfiles garantizan una
+                                            hermeticidad total.</li>
+                                        <li>Las distintas configuraciones de cristal permitidas en nuestros sistemas
+                                            ofrecen soluciones en
+                                            condiciones climáticas extremas.</li>
+                                        <li>Los mecanismos de cierre y apertura son ajustados acorde a los parámetros
+                                            requeridos.</li>
+
+                                    </ul>
+                                    </p>
+                                    <p>
+                                    <h4>Configuración</h4>
+                                    <ol style="color: #000;">
+                                        <li>Marco principal</li>
+                                        <li>Marco ventana</li>
+                                        <li>Maneta</li>
+                                        <li>Sistema cerradura</li>
+                                        <li>Cubre pijas</li>
+                                        <li>Sello</li>
+                                        <li>Reductor</li>
+                                        <li>Bisagra</li>
+                                    </ol>
+                                    </p>
+                                    <div class="container mb-5">
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <span>ForView 28 / Ventana batiente</span> <br>
+                                                <img style="width: 20rem;" src="/images/abatible28-v.png" alt="">
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf"
+                                    download="03-Batiente.pdf">
+                                    Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- <section class="folleto">
+    <div class="container">
+        <div class="row mb-5 animate-box" data-animate-effect="fadeInUp">
+            <div class="col-md-6">
+                <img src="/images/folleto.png" alt="">
+            </div>
+            <div class="col-md-6">
+                <h2>FOLLETO TÉCNICO</h2>
+                <span>Puede conocer sus características técnicas descargando el folleto técnico de ForView</span>
+                <br><br>
+
+                <a type="button" class="btn btn-dark mt-3" href="/pdf/03-Batiente.pdf" download="03-Batiente.pdf">
+                    Descargar
+                </a>
+            </div>
+        </div>
+    </div>
+</section> -->
---
+++ proteccion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include('includes/head.php');
+?>
+
+<body>
+    <div class="content-wrapper">
+        <?php
+         include('includes/header.php');
+         include('modules/antihuracan.php');
+         include('includes/footer.php');
+        ?>
+    </div>
+    <?php
+        include('includes/scripts.php');
+    ?>
+</body>
+
+</html>
