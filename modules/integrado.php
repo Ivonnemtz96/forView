d34f17638a450976ddc9ff46da796c3b176e09ad
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-05-02 13:25:04 -0700

    Integrado 54 OK

--- modules/integrado.php
+++ modules/integrado.php
@@ -13,48 +13,33 @@
 <section class="project-page section-padding" style="padding-bottom: 0px;">
     <div class="container-fluid p-5">
         <div class="row align-items-center">
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <span>Producto</span>
                 <h2>Integrado</h2>
                 <h2></h2>
-                <p>
-                    El sistema corredizo permite acceder a una amplia vista panorámica, con distintas configuraciones de
-                    hojas y aperturas. Su diseño minimalista permite embutir el marco en el piso sin generar un cambio
-                    de nivel, el cual al realizar una transición entre dos espacios pasa desapercibido al tacto, y a su
-                    vez, es posible almacenar las hojas en un cajón de muro, despejando completamente el espacio.
+                <p style="text-align: justify;">
+                    Un sistema integrado de puertas corredizas es una solución de alta calidad y rendimiento diseñada
+                    para elementos monumentales que requieren dimensiones personalizadas. <br> Con una perfilería
+                    robusta y
+                    un diseño arquitectónico sofisticado, permite una vista limpia y una gran sensación en su operación.
+                    Además, ofrece la posibilidad de colocar distintas configuraciones de cristal con un espesor máximo,
+                    lo que permite alcanzar altas prestaciones térmicas y acústicas. Este tipo de sistemas también
+                    suelen ocultar la tornillería y sellar completamente los cristales, lo que aporta elegancia,
+                    robustez y practicidad a la solución. Con un acabado anodizado de alta calidad, los sistemas
+                    integrados de puertas corredizas son una excelente opción para cualquier proyecto arquitectónico que
+                    requiera una solución sofisticada y eficiente en términos de espacio y funcionalidad.
                 </p><br>
-                <p>
-                    De igual forma cuenta con soluciones anti huracán, contemplando la configuración de los refuerzos
-                    y dimensiones del elemento, su facilidad de movimiento es posible debido a que los rodamientos son
-                    de acero inoxidable fabricados en ForView. <br> <br>
-                    El diseño arquitectónico minimalista de los canceles permiten ocultar de la vista la tornillería, al
-                    igual que sellar completamente los cristales, con espesores anodizados para los perfiles desde 20
-                    micras. Todas estas características aportan elegancia, robustez y practicidad a estas soluciones de
-                    puertas corredizas.
-                </p>
             </div>
-            <div class="col-md-6">
-                <div class="fotorama" data-width="100%" data-ratio="800/600" data-minwidth="400" data-maxwidth="1000"
-                    data-minheight="300" data-maxheight="100%">
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/43.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/44.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chilenos.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno2.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno3.jpg" alt="">
-                    </div>
-                    <div class="portfolio-item"> <img class="img-fluid" src="/images/projects/chileno4.jpg" alt="">
-                    </div>
-
+            <div class="col-md-4">
+                <div class="fotorama" data-loop="true" data-autoplay="true" data-autoplay="1000">
+                    <img class="img-fluid" src="/images/projects/24.jpg" alt="">
+                    <img class="img-fluid" src="/images/projects/25.jpg" alt="">
+                    <img class="img-fluid" src="/images/projects/62.jpg" alt="">
                 </div>
-
             </div>
         </div>
     </div>
-    <div class="container p-5">
+    <div hidden class="container p-5">
         <!-- project slider -->
         <div class="row mt-5">
             <h2><b>Proyectos</b></h2>
@@ -123,12 +108,12 @@
 </section>
 
 <section class="section-padding" style="padding-top: 0px;">
-    <div class="container" >
+    <div class="container">
         <div class="row mb-5 animate-box" data-animate-effect="fadeInUp"></div>
         <!-- Serie 38 -->
         <div id="38"></div>
         <div class="col-md-12" style="margin-top:10rem;">
-            <ul class="accordion-box clearfix" >
+            <ul class="accordion-box clearfix">
                 <li class="accordion block <? echo $var38_1;?>">
                     <div class="acc-btn <? echo $var38_2;?>">
                         <h2>ForView 38</h2>
@@ -203,20 +188,23 @@
             <ul class="accordion-box clearfix" style="margin-top:10rem;">
                 <li class="accordion block <? echo $var54_1;?>">
                     <div class="acc-btn  <? echo $var54_2;?>">
-                        <h2>ForView 54</h2>
+                        <h2>ForView 54 Pivotante- Corredera</h2>
                     </div>
                     <div class="acc-content" style="<? echo $var54_3;?>;">
                         <div class="content">
                             <div class="text">
                                 <p style="color: #000;">
-                                El sistema corredizo permite acceder a una amplia vista panorámica, con distintas
-                                    configuraciones de
-                                    hojas y aperturas. Su diseño minimalista permite embutir el marco en el piso sin
-                                    generar un cambio
-                                    de nivel, el cual al realizar una transición entre dos espacios pasa desapercibido
-                                    al tacto, y a su
-                                    vez, es posible almacenar las hojas en un cajón de muro, despejando completamente el
-                                    espacio.
+                                    El sistema integrado FV-54 diseñado para elementos monumentales cuyas dimensiones
+                                    sobrepasan por mucho los estándares comerciales. <br>
+                                    Con perfilería robusta permitiendo una
+                                    vista limpia con un mínimo porcentaje de
+                                    obstrucción y gran sensación en su
+                                    operación, con la posibilidad de colocar
+                                    distintas configuraciones de cristal
+                                    llegando a ocupar un espesor máximo de
+                                    54mm, esto en conjunto con la ruptura de
+                                    puente térmico lleva las prestaciones
+                                    térmicas y acústicas al más alto nivel.
                                 </p>
                                 <p>
                                 <h4>Configuración</h4>
@@ -229,24 +217,25 @@
                                     <li>Riel</li>
                                     <li>Tapa marco</li>
                                     <li>Rodamiento</li>
-                                    <li>Tirador fagal</li>
+                                    <li>Tirador doble fagal</li>
                                     <li>Chapa</li>
                                     <li>Tapcon hexagonal</li>
-                                    <li>Tapa Cubre pijas</li>
+                                    <li>Vinil guardapolvo</li>
+                                    <li>Maneta</li>
                                 </ol>
                                 </p>
                                 <div class="container mb-5">
                                     <div class="row">
                                         <div class="col-md-12">
                                             <span>ForView 54 Integrado</span> <br>
-                                            <img style="width: 20rem;" src="/images/forview38.png" alt="">
+                                            <img style="width: 20rem;" src="/images/mapas/integrado-54.png" alt="">
 
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <a type="button" class="btn btn-dark mt-3" href="/pdf/01-Corredizas.pdf"
-                                download="01-Corredizas.pdf">
+                            <a type="button" class="btn btn-dark mt-3" href="/pdf/Integrado-FV54.pdf"
+                                download="Integrado-FV54.pdf">
                                 Descargar hoja de datos <i class="ti-arrow-circle-down"></i>
                             </a>
                         </div>
@@ -254,7 +243,7 @@
                 </li>
             </ul>
         </div>
-      
+
     </div>
     </div>
 </section>
